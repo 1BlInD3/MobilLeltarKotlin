50c18c8d44793e13d80eb66905ec98a3e6d4ad4e
Attila Bálind <attila.balind.repo@gmail.com>
2021-10-14 09:10:37 +0200

    javított navigation bar kezelés a contianeren

--- app/src/main/java/com/fusetech/mobilleltarkotlin/activity/MainActivity.kt
+++ app/src/main/java/com/fusetech/mobilleltarkotlin/activity/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.fusetech.mobilleltarkotlin.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.KeyEvent
-import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.fusetech.mobilleltarkotlin.R
 import com.fusetech.mobilleltarkotlin.dataItems.RaktarAdat
 import com.fusetech.mobilleltarkotlin.fragments.*
 import com.fusetech.mobilleltarkotlin.showMe
 import com.honeywell.aidc.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 
@@ -337,13 +342,16 @@ class MainActivity : AppCompatActivity(), BarcodeReader.BarcodeListener,
         Log.d(TAG, "timerCancel: Start")
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                // or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+       // window.setDecorFitsSystemWindows(false) api > 30
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window,container).let { controller ->
+            //controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
